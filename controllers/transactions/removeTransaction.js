614889ea0acb37e72600f90338bcfca07cd82b75
Sergey-Proskurnin <proskurnin@ua.fm>
2021-10-11 19:11:06 +0300

    Fix controller delete transaction and route-transation/period

--- controllers/transactions/removeTransaction.js
+++ controllers/transactions/removeTransaction.js
@@ -1,5 +1,4 @@
 const { Transaction } = require('../../model/transaction');
-const User = require('../../repositories/users');
 
 const {
   HttpCode: { OK, NOT_FOUND },
@@ -7,19 +6,15 @@ const {
 
 const removeTransaction = async (req, res, next) => {
   try {
-    const userId = req.user._id;
-    const userBalance = req.body.balance;
-    const resultBalance = await User.createBalance(userId, userBalance);
     const { id } = req.params;
     const result = await Transaction.findByIdAndRemove({ _id: id });
-    if (!result && !resultBalance) {
+    if (!result) {
       return res.status(NOT_FOUND).json({
         message: 'Not found',
       });
     }
-    const { balance } = resultBalance;
-    res.status(OK).json({
-      balance,
+     res.status(OK).json({
+      message: 'Your transaction was deleted!'
     });
   } catch (error) {
     next();
