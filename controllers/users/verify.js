96227a115c71041fa4bdfceb784052802af29e88
Sergey-Proskurnin <proskurnin@ua.fm>
2021-10-19 02:12:12 +0300

    fix index.html

--- controllers/users/verify.js
+++ controllers/users/verify.js
@@ -1,21 +1,17 @@
-import MessageView from '..//..//views/PageMessageView/PageMessageView'
-
 const Users = require('../../repositories/users');
+const path = require('path');
 const {
   HttpCode: { OK, NOT_FOUND },
 } = require('../../helpers');
+
 
 const verify = async (req, res, next) => {
   try {
     const user = await Users.findByVerifyToken(req.params.verificationToken);
     if (user) {
       await Users.updateTokenVerify(user.id, true, null);
-      return res
-        .status(OK)
-        .send(
-            <MessageView/>
-          // '<h1>Congratulations, your email has been successfully verified, go to the <a href="https://kapusta-fsd28.netlify.app">login</a> page in the phone book and enter your email and password.</h1>',
-        );
+      return res.status(OK)
+       .sendFile(path.join(__dirname, '/index.html'));
     }
     return res
       .status(NOT_FOUND)
